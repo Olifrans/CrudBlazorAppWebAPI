59cb613de09ab84a6c12c5c9eee5cc31cf26b604
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2021-01-30 19:20:19 -0300

    Initial commit

---
+++ CrudBlazorAppWebAPI/Models/Produtos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudBlazorAppWebAPI.Models
+{
+    public class Produtos
+    {		
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O Nome do Produto é Obrigatório")]
+        [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O Tipo do Produto é Obrigatório")]
+		[Display(Name = "Tipo do Produto")]
+        public string Tipo { get; set; }
+		
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CrudBlazorAppWebAPI
+ Crud Aprendizado
